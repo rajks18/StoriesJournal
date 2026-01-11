d9dfe2c55fcd2eaf63f221449e160b70e6e24427
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-11 19:46:51 +0600

    Update FeaturedPosts.jsx

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -93,7 +93,7 @@ export default function FeaturedPosts() {
             className="w-10 h-10 flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-full transition-all duration-300"
             aria-label="Previous page"
           >
-            <ChevronLeft className="w-5 h-5 text-gray-700" />
+            <ChevronLeft className="w-5 h-5 " />
           </button>
           <button
             onClick={nextPage}
