e6499de11681d1e7c87eefaffd07978886aa8cea
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-11 19:45:50 +0600

    Update FeaturedPosts.jsx

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -70,7 +70,7 @@ export default function FeaturedPosts() {
   const gridPosts = displayedPosts.slice(1);
 
   return (
-    <div className=" max-w-7xl mx-auto px-6 lg:px-8">
+    <div className="w-full max-w-7xl mx-auto px-6 lg:px-8">
       {/* Header */}
       <div className="flex items-center justify-between mb-8">
         <div>
